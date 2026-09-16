e76321a07cee857d648f12875f0dfcac33c9a05a
CompotScript <vajdies@gmail.com>
2026-09-16 17:42:29 +0300

    Update TestEnoisV1.lua

--- TestEnoisV1.lua
+++ TestEnoisV1.lua
@@ -520,19 +520,18 @@ bl.Font=Enum.Font.GothamMedium;bl.TextSize=15
 bl.TextColor3=WHITE;bl.TextTransparency=1;bl.Text=BTN
 bl.ZIndex=6;bl.Parent=btn
 task.spawn(function()
-task.wait(0.35)
-for _,l in ipairs(letters) do
-tw(l,0.4,{TextTransparency=0},Enum.EasingStyle.Cubic)
-tw(l,0.5,{Position=UDim2.fromOffset(0,0)})
-task.wait(0.1)
-end
+    task.wait(0.35)
+    for _, l in ipairs(letters) do
+        tw(l, 0.4, {TextTransparency = 0}, Enum.EasingStyle.Cubic) 
+        tw(l, 0.5, {Position = UDim2.fromOffset(0, 0)})
+        task.wait(0.1)
+    end
 end)
 task.wait(0.4)
-tw(wl,0.6,{TextTransparency=0})
-tw(btn,0.6,{BackgroundTransparency=0.85})
-tw(bs,0.6,{Transparency=0.4})
-tw(bl,0.6,{TextTransparency=0})
-end)
+tw(wl, 0.6, {TextTransparency = 0})
+tw(btn, 0.6, {BackgroundTransparency = 0.85})
+tw(bs, 0.6, {Transparency = 0.4})
+tw(b1, 0.6, {TextTransparency = 0}
 btn.MouseButton1Click:Connect(function()
 tw(wl,0.3,{TextTransparency=1})
 tw(bl,0.3,{TextTransparency=1})
